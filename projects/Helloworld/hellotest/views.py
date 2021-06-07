8692a6f46257e61b04f11854584077f5f2658d63
ANKUR <ankurvermaaxz@gmail.com>
2021-06-07 10:42:37 +0530

    Float Number Adder - POST REQUEST Example - CSRF

--- projects/Helloworld/hellotest/views.py
+++ projects/Helloworld/hellotest/views.py
@@ -6,9 +6,9 @@ def home(request):
     return render(request, 'home.html', {'name': 'Ankur'})
 
 
-def add(request):
-    num1 = float(request.GET['num1'])
-    num2 = float(request.GET['num2'])
-    x = int(request.GET['places'])
+def add(request): #GET REQUEST
+    num1 = float(request.POST['num1'])
+    num2 = float(request.POST['num2'])
+    x = int(request.POST['places'])
     res = str(round((num1 + num2), x))
     return render(request, 'result.html', {'sum': res})
