e2bdf90d60617fb4ed81cf6ffdad0daf044df679
ANKUR <ankurvermaaxz@gmail.com>
2021-06-07 10:28:25 +0530

    Float Number Adder - GET REQUEST Example

--- projects/Helloworld/hellotest/views.py
+++ projects/Helloworld/hellotest/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-import os
-from django.http import HttpResponse
 
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {'name': 'Ankur'})
+
+
+def add(request):
+    num1 = float(request.GET['num1'])
+    num2 = float(request.GET['num2'])
+    x = int(request.GET['places'])
+    res = str(round((num1 + num2), x))
+    return render(request, 'result.html', {'sum': res})
