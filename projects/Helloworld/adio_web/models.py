de38ed1e1e29363252fdbe33a077c872144be389
ANKUR <ankurvermaaxz@gmail.com>
2021-06-08 10:12:17 +0530

    Dynamic Data for HTML (Ads)

--- projects/Helloworld/adio_web/models.py
+++ projects/Helloworld/adio_web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+# creating model for advertisement
+class ads:
+    id: int  # Primary ID for db
+    name: str  # Advertisement/ Classified Name
+    desc: str  # Ad Desc
+    img: str  # Image of Ad
--- projects/Helloworld/adio_web/views.py
+++ projects/Helloworld/adio_web/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import ads
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html',{'about':'This About Us is Dynamic'})
+    ad1 = ads()
+    ad1.name = "Dynamic Ad"
+    ad1.desc = "This Ad is Dynamic, Django is doing this shit"
+    ad1.img = "https://upload.wikimedia.org/wikipedia/commons/2/2a/New_Logo_AD.jpg"
+    return render(request, 'index.html', {'about': 'This About Us is Dynamic',
+                                          'ad1': ad1})
